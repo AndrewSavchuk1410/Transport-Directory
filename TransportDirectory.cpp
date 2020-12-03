9c8aa4f6569da56d57386787ac5c06868fbecbdb
AndrewSavchuk1410 <48767019+AndrewSavchuk1410@users.noreply.github.com>
2020-12-03 22:06:57 +0200

    requests

--- TransportDirectory.cpp
+++ TransportDirectory.cpp
@@ -1,20 +1,28 @@
-﻿// TransportDirectory.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include "descriptions.h"
+#include "json.h"
+#include "requests.h"
+#include "sphere.h"
+#include "transport_catalog.h"
+#include "utils.h"
 
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+int main() {
+    const auto input_doc = Json::Load(cin);
+    const auto& input_map = input_doc.GetRoot().AsMap();
+
+    const TransportCatalog db(
+        Descriptions::ReadDescriptions(input_map.at("base_requests").AsArray()),
+        input_map.at("routing_settings").AsMap()
+    );
+
+    Json::PrintValue(
+        Requests::ProcessAll(db, input_map.at("stat_requests").AsArray()),
+        cout
+    );
+    cout << endl;
+
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
